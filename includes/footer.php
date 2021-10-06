da77ffcdf51097f5f6dee40157fd0a1a3ba75670
vasireddyujwala <92040231+vasireddyujwala@users.noreply.github.com>
2021-10-06 21:04:35 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Thank You</b>
+      </div>
+      <strong>By C_FS02 </strong>
+    </div>
+    <!-- /.container -->
+</footer>
